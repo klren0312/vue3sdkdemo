b395e4bdc201d8dc7e37203a2f2df4c34d91780a
klkl <kuanglong0312@sina.com>
2024-05-18 09:36:02 +0800

    feat: 支持传入参数

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import './style.css'
 import App from './components/HelloWorld.vue'
 
-function renderVueComponentToDOM(domElement) {
-  createApp(App).mount(domElement)
+function renderVueComponentToDOM(domElement, msg) {
+  const TheComponent = h(App, {
+    msg
+  }, null)
+  createApp(TheComponent).mount(domElement)
 }
 
 export default renderVueComponentToDOM
